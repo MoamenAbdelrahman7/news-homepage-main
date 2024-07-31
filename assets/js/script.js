1fdf20c40346ad7bf863e7542233ba402dc09b5c
Moamen Abdelrahman Abdallah <141651324+MoamenAbdelrahman7@users.noreply.github.com>
2024-07-31 05:28:29 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,14 @@ MenuBt.addEventListener("click", () => {
     console.log(navBar.style.display);
     if (navBar.style.display === "" || navBar.style.display === "none"){
         navBar.style.display="flex";
-        MenuBt.querySelector("img").src="../assets/images/icon-menu-close.svg";
+        MenuBt.querySelector("img").src="../images/icon-menu-close.svg";
         MenuBt.querySelector("img").style.width="25px";
         navBar.style.animationDirection="normal";
         // prevent window from scrolling while nav menu id opend
         document.body.style.overflow="hidden";
     }
     else{
-        MenuBt.querySelector("img").src="../assets/images/icon-menu.svg";
+        MenuBt.querySelector("img").src="../images/icon-menu.svg";
         MenuBt.querySelector("img").style.width="40px";
         document.body.style.overflow="";
         navBar.style.animationDirection="reverse";
